0315fd0c53b6e62998c4c48ed7c94c403a0490e8
MathFreitas <matheus.freitas_santos@outlook.com>
2021-11-06 00:49:53 -0300

    implementação da api do google e do redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react'; 
+import { Provider } from 'react-redux';
 import {ThemeProvider} from 'styled-components';
+import { Reset } from 'styled-reset'
+
 import Home from './pages/Home';
 import theme from './theme';
-import { Reset } from 'styled-reset'
+import store from './pages/Home';
 
 
 
 function App() {
-  return <ThemeProvider theme={theme}>
-    <Reset />
-    <Home />
-  </ThemeProvider>
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Reset />
+        <Home />
+      </ThemeProvider>
+    </Provider>
+  )
 }
 
 export default App;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,62 @@
-import React from "react"; 
-import { Container } from "./styles";
+import React, { useState } from "react"; 
+import Slider from "react-slick";
+import TextField, {Input} from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
-const Home = () => <Container>Hello World</Container>;
+import logo from '../../assets/logo.svg';
+import restaurantes from '../../assets/restaurante-fake.png';
+import { Card, RestaurantCard, Modal, Map } from "../../components";
 
+import { Container, Search, Logo, Wrapper, CarouselTitle, Carousel } from "./styles";
+
+
+const Home = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [query, setQuery] = useState(null);
+    const [modalOpened, setModalOpened] = useState(false);
+    
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+    };
+
+    function handleKeyPress(e) {
+        if (e.key === 'Enter') {
+            setQuery(inputValue);
+        }
+    }
+
+    return (
+        <Wrapper>
+            <Container>
+                <Search>
+                    <Logo src={logo} alt="Logo do restaurante" />
+                    <TextField
+                        label="Pesquisar Restaurantes"
+                        outlined
+                        trailingIcon={<MaterialIcon role="button" icon="search"/>}>
+                        <Input 
+                            value={inputValue} 
+                            onKeyPress={handleKeyPress} 
+                            onChange={(e) => setInputValue(e.target.value)} />
+                    </TextField>
+                    <CarouselTitle>Na sua Área</CarouselTitle>
+                    <Carousel {...settings} >
+                        <Card photo={restaurantes} title="nome sei lá"/>
+                        <Card photo={restaurantes} title="nome sei lá"/>
+                        <Card photo={restaurantes} title="nome sei lá"/>
+                    </Carousel>
+                </Search>
+                <RestaurantCard />
+            </Container>
+            <Map query={query} />
+            {/*<Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)} />*/}
+        </Wrapper>
+        
+    );
+};  
 export default Home;
