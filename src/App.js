2bba5ecd2f94def86552f6045390a9563256a5cb
MathFreitas <matheus.freitas_santos@outlook.com>
2021-11-08 18:19:45 -0300

    remove o arquivo .env e refatorando partes com erro

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react'; 
+import React from 'react';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
 
-import {ThemeProvider} from 'styled-components';
-import { Reset } from 'styled-reset'
-
-import Home from './pages/Home';
+import store from './redux/store';
 import theme from './theme';
-import store from './pages/Home';
-
-
+import Home from './pages/Home';
 
 function App() {
   return (
+    <Provider store={store}>
       <ThemeProvider theme={theme}>
         <Reset />
         <Home />
       </ThemeProvider>
-  
-  )
+    </Provider>
+  );
 }
 
 export default App;
